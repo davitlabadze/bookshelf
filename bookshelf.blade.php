b892eb367d76f7c93119ec1ead93c8dca416a0c1
Davit Labadze <46887782+davitlabadze@users.noreply.github.com>
2022-09-10 15:45:34 +0400

    create bookshelf.blade.php
    one page of shelves where on mouseover  each shelf shows the author's name and a button to go to that author's book page

---
+++ bookshelf.blade.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>BookShelf</title>
+    @vite('resources/css/app.css')
+
+</head>
+
+<body>
+
+    <div class="container mx-auto text-center ">
+        <div class="">
+            <table class="absolute top-0  border-blue-800 bg-[url('../../public/image/shelf.png')]  bg-contain ">
+                <tbody class="grid grid-cols-3 ml-6">
+                    <tr class='ml-2 py-11 hover:bg-gray-200'>
+                        <td class="p-20 ">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+                    </tr>
+                    <tr class='ml-2 py-11 hover:bg-gray-200'>
+                        <td class="p-20 ">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+                    </tr>
+                    <tr class='ml-2 py-11 hover:bg-gray-200'>
+                        <td class="p-20 ">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+                    </tr>
+                    <tr class='ml-2 py-11 hover:bg-gray-200'>
+                        <td class="p-20 ">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+                    </tr>
+                    <tr class='ml-2 py-11 hover:bg-gray-200'>
+                        <td class="p-20 ">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+                    </tr>
+                    <tr class='ml-2 py-11 hover:bg-gray-200'>
+                        <td class="p-20 ">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+                    </tr>
+                    <tr class='ml-2 py-11 hover:bg-gray-200'>
+                        <td class="p-20 ">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+                    </tr>
+                    <tr class='ml-2 py-11 hover:bg-gray-200'>
+                        <td class="p-20 ">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+                    </tr>
+                    <tr class='ml-2 py-11 hover:bg-gray-200'>
+                        <td class="p-20 ">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+                    </tr>
+                    <tr class='ml-2 py-11 hover:bg-gray-200'>
+                        <td class="p-20 ">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+                    </tr>
+                    <tr class='ml-2 py-11 hover:bg-gray-200'>
+                        <td class="p-20 ">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+                    </tr>
+                    <tr class='ml-2 py-11 hover:bg-gray-200'>
+                        <td class="p-20 ">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+                    </tr>
+                    <tr class='ml-2 py-11 hover:bg-gray-200'>
+                        <td class="p-20 ">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+                    </tr>
+                    <tr class='ml-2 py-11 hover:bg-gray-200'>
+                        <td class="p-20 ">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</body>
+
+</html>
